2218fdf3a06c8232ab7621fed2c300def6efcb3f
Isaac Kim <isjikim@gmail.com>
2019-02-08 19:14:40 -0800

    participation

--- analysis.R
+++ analysis.R
@@ -6,20 +6,39 @@ library("tidyr")
 library("ggmap")
 
 # Load in your data
-
+evictions <- read.csv("data/Eviction_Notices.csv", stringsAsFactors = F)
 # Compute some values of interest and store them in variables for the report
 
 # How many evictions were there?
+num_evictions <- nrow(evictions)
+num_features <- ncol(evictions)
 
 # Create a table (data frame) of evictions by zip code (sort descending)
+by_zip <- evictions %>%
+  group_by(Eviction.Notice.Source.Zipcode) %>%
+  count() %>%
+  arrange(-n) %>%
+  ungroup() %>%
+  top_n(10, wt = n)
 
 # Create a plot of the number of evictions each month in the dataset
+by_month <- evictions %>%
+  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
+  mutate(month = floor_date(date, unit = "month")) %>%
+  group_by(month) %>%
+  count()
 
 # Store plot in a variable
+ggplot(data = by_month) +
+  geom_line(mapping = aes(x = month, y = n)) +
+  labs(x = "Date", y = "Number of Evictions", title = "Evictions over time in SF")
 
 # Map evictions in 2017 
 
 # Format the lat/long variables, filter to 2017
+evictions_2017 <- evictions %>%
+  mutate(date = as.Date(File.Date, format = "%m/%d/%y")) %>%
+  lat = as.numeric(gsub("\\(", "", lat))
 
 # Create a maptile background
 
